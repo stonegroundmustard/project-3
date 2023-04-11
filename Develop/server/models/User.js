aa673ec179735c8acc089c54b70754416ea058d1
Fox Stokes <foxvalentine001@gmail.com>
2023-04-10 19:11:48 -0500

    adding utils files and package.json

--- Develop/server/models/User.js
+++ Develop/server/models/User.js
@@ -46,7 +46,7 @@ userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-// when we query a user, we'll also get another field called `movieCount` with the number of saved books we have
+// when we query a user, we'll also get another field called `movieCount` with the number of saved movies we have
 userSchema.virtual('movieCount').get(function () {
   return this.savedMovies.length;
 });
